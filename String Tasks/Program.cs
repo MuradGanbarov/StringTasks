ffa1e9c08b4ab6d73426ee811fb122eebffe0131
MuradGanbarov <tu6xfxlrk@code.edu.az>
2023-10-09 23:59:30 +0400

    String Tasks

---
+++ String Tasks/Program.cs
@@ -0,0 +1,55 @@
+﻿//using System.Text;
+
+//namespace StringTask
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine(CorrectString("        EverytHing  i neeD,    is a CuP   of       CoFFee    "));
+
+//        }
+
+//        static string CorrectString(string text)
+//        {
+//            text = text.Trim().ToLower();
+
+//            StringBuilder sb = new StringBuilder();
+
+//            sb.Append(char.ToUpper(text[0]));
+
+//            for (int i = 1; i < text.Length; i++)
+//            {
+//                if (text[i] != ' ')
+//                {
+//                    if (text[i - 1] == ' ')
+//                    {
+
+//                        sb.Append(char.ToUpper(text[i]));
+//                    }
+//                    else sb.Append(text[i]);
+
+//                }
+
+//                if (text[i] == ' ')
+//                {
+
+//                    sb.Append(' ');
+
+//                    while (text[i] == ' ')
+//                    {
+//                        i++;
+//                    }
+//                    i--;
+
+//                }
+//            }
+
+//            return sb.ToString();
+
+
+//        }
+//    }
+
+
+//}
